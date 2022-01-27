367de023bfe696625bef28c95aa132bd79d4eb88
Lauren Suguro <lsuguro@Laurens-MacBook-Pro.local>
2022-01-27 17:53:01 -0500

    added steps

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Your Code Here
+//User enters name
+let userName = window.prompt("Please enter your name:");
+let userScore = 0;
+
+//Function to play the game and display trivia questions
+function playGame() {
+  for (let i = 0; i < questions.length; i++) {
+    let question = questions[i];
+    let userAnswer = window.prompt(question.text);
+    if (userAnswer === question.correctAnswer) {
+      userScore = userScore + 10;
+    }
+  }
+  window.alert("Your score is: " + userScore);
+}
+
+playGame();
